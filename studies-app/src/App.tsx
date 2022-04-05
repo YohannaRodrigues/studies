c77166f9bacbd3d3cff0c1238ab7531a74f9559a
Yohanna Rodrigues <97924905+YohannaRodrigues@users.noreply.github.com>
2022-04-04 23:01:23 -0300

    Primeiros passos com react e typescript

---
+++ studies-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Formulario from "./components/form";
+import Lista from "./components/list";
+
+function App() {
+  return (
+    <div className="App">
+      <Formulario />
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
---
+++ studies-app/src/components/button/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Botão extends React.Component {
+  render() {
+    return <button>Botão</button>;
+  }
+}
+
+export default Botão;
