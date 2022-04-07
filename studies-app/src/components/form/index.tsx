cf08b9b3843d384e2fec5b71f3813a3e15078bd2
Yohanna Rodrigues <97924905+YohannaRodrigues@users.noreply.github.com>
2022-04-06 22:37:05 -0300

    useState

--- studies-app/src/components/form/index.tsx
+++ studies-app/src/components/form/index.tsx
@@ -3,15 +3,31 @@ import Botão from "../button";
 import style from "./form.module.scss";
 
 class Formulario extends React.Component {
+  state = {
+    tarefa: "",
+    tempo: "00:00",
+  };
+
+  adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    console.log("state", this.state);
+  }
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form
+        className={style.novaTarefa}
+        onSubmit={this.adicionarTarefa.bind(this)}
+      >
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo </label>
           <input
             type="text"
             name="tarefa"
             id="tarefa"
+            value={this.state.tarefa}
+            onChange={(evento) =>
+              this.setState({ ...this.state, tarefa: evento.target.value })
+            }
             placeholder="O que você quer estudar?"
             required
           />
@@ -22,6 +38,10 @@ class Formulario extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.tempo}
+            onChange={(evento) =>
+              this.setState({ ...this.state, tempo: evento.target.value })
+            }
             id="tempo"
             min="00:00:00"
             max="01:30:00"
